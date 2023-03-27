3bee06c0b9283e4b1eed3d9a4a4eee195f2ac78a
Ian Ansell <ian.ansell.uk@gmail.com>
2023-03-27 16:08:51 +0100

    pass container ID to views using render_template

--- haas/views.py
+++ haas/views.py
@@ -7,10 +7,20 @@ from haas import app
 from haas.generator import generate_hex
 
 
+container_id = {
+        "ua": "GTM-5S9XWS6",
+        "ga4": "GTM-NBCMVLT",
+    }
+
 @app.get("/")
 def home():
     """Show home page."""
-    return render_template("index.html", title="Home")
+    ga_version = request.args.get("type", default="ga4")
+    return render_template(
+        "index.html", 
+        container_id=container_id[ga_version], 
+        title="Home",
+    )
 
 
 @app.get("/api")
@@ -23,10 +33,6 @@ def api():
 def analytics():
     """Render a hexagram reading with the submitted number of hexagrams."""
     ga_version = request.args.get("type", default="ga4")
-    container_id = {
-        "ua": "GTM-5S9XWS6",
-        "ga4": "GTM-NBCMVLT",
-    }[ga_version]
 
     if request.method == "POST":
         input = request.form["hex_count"] or "6"
@@ -36,7 +42,7 @@ def analytics():
 
     return render_template(
         "reading.html",
-        container_id=container_id,
+        container_id=container_id[ga_version],
         title="Hexagrams",
         hexagrams=generate_hex(num),
     )
@@ -45,4 +51,8 @@ def analytics():
 @app.get("/cookies")
 def cookies():
     """Render the cookies page."""
-    return render_template("cookies.html")
+    ga_version = request.args.get("type", default="ga4")
+    return render_template(
+        "cookies.html", 
+        container_id=container_id[ga_version],
+    )
